0d0fba6bb2aaaa797ba7b83d13c4c66bd3a8fc87
hyunwoo_ <mrsao96@gmail.com>
2020-06-10 21:34:31 +0900

    검색 화면에 최신 메뉴나 추천 메뉴를 롤링 배너 형식으로 만들기위해서 뷰페이져 이용 구현. 근데 시작하면 검색 프래그먼트에서 뉴스피드 프래그먼트의 텍스트 뷰가 보이는 문제가 발생. 검색 프래그먼트 클래스에서 frg1,2,3 각각에 URL이나 사진을 연결할 경우 일정 시간마다 슬라이드 되는 배너 광고를 구현할 수 있음

--- app/src/main/java/com/baekdev/sip/SearchFragment.java
+++ app/src/main/java/com/baekdev/sip/SearchFragment.java
@@ -1,23 +1,115 @@
 package com.baekdev.sip;
 
 import android.os.Bundle;
-
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.PagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-public class SearchFragment extends Fragment {
 
-    public SearchFragment() {
-        // Required empty public constructor
+
+
+public class SearchFragment extends Fragment {
+    int i=0;
+    ViewPager viewPager;
+    private pageFrag_1 fragment1;
+    private pageFrag_2 fragment2;
+    private pageFrag_3 fragment3;
+    public SearchFragment(){
+
     }
 
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_search, container, false);
+        View view = inflater.inflate(R.layout.fragment_search, container, false);
+        fragment1=new pageFrag_1();
+        fragment2=new pageFrag_2();
+        fragment3=new pageFrag_3();
+        viewPager=(ViewPager)view.findViewById(R.id.viewPager);
+        viewPager.setAdapter(new PagerAdapter(getChildFragmentManager()));
+        viewPager.setCurrentItem(0);
+        return view;
     }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+
+    }
+    private class PagerAdapter extends FragmentPagerAdapter{
+        public PagerAdapter(FragmentManager fm){
+            super(fm);
+            getItem(0);
+        }
+        public Fragment getItem(int position){
+            if(position==0)
+            {
+                return fragment1;
+            }else if(position==1){
+                return fragment2;
+            }else {
+                return fragment3;
+            }
+        }
+        public int getCount(){
+            return 3;
+        }
+
+    }
+
+
+
+
 }
+
+
+
